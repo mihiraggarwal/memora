ee130b09a90f8efcded02dc96c21a6d6769ca67e
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2025-01-01 12:13:33 +0530

    bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,12 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNav(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.mic),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
@@ -163,4 +169,41 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({Key? key}) : super(key: key);
+
+  @override
+  _BottomNavState createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      items: [
+        BottomNavigationBarItem(
+          label: "Home",
+            icon: Icon(Icons.home)
+        ),
+        BottomNavigationBarItem(
+            label: "Other",
+            icon: Icon(Icons.accessible)
+        ),
+        BottomNavigationBarItem(
+          label: "Quiz",
+          icon: Icon(Icons.quiz)
+        ),
+        BottomNavigationBarItem(
+          label: "Profile",
+            icon: Icon(Icons.account_circle_rounded)
+        )
+      ],
+    );
+  }
+}
+
 
