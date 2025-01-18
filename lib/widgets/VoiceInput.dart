c0bf9c22547f58d1164f87c3f3838bed3e3885fa
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2025-01-18 23:32:18 +0530

    azure openai

--- lib/widgets/VoiceInput.dart
+++ lib/widgets/VoiceInput.dart
@@ -1,5 +1,9 @@
+import "dart:convert";
+
 import "package:firebase_auth/firebase_auth.dart";
 import "package:cloud_firestore/cloud_firestore.dart";
+import "package:http/http.dart" as http;
+import "package:intl/intl.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 import "package:flutter_dotenv/flutter_dotenv.dart";
@@ -23,13 +27,18 @@ class _VoiceInputState extends State<VoiceInput> {
   String lang = "en-US";
   late AzureSpeechRecognition _speechAzure;
 
+  String endpoint = dotenv.env['OAI_ENDPOINT']!;
+  String oaiKey = dotenv.env['OAI_KEY']!;
+
   bool voiceActive = false;
   String transcription = '';
 
   Future<void> statementPush(text) async {
     DateTime now = DateTime.now();
+    String formattedDate = DateFormat().format(now);
+
     Map<String, dynamic> upload = {
-      "datetime": now,
+      "datetime": formattedDate,
       "content": text
     };
 
@@ -43,12 +52,69 @@ class _VoiceInputState extends State<VoiceInput> {
     });
   }
 
+  Future<List> questionPull() async {
+    final uid = auth.currentUser?.uid;
+    List<dynamic> logs = [];
+
+    await firestore.collection("users").where("uid", isEqualTo: uid).limit(1).get().then((QuerySnapshot snapshot) {
+      for (var doc in snapshot.docs) {
+        logs = doc["logs"];
+      }
+    });
+
+    return logs;
+  }
+
+  Future<String> runLLM(text, logs) async {
+    final uid = auth.currentUser?.uid;
+    final Map<String, dynamic> body = {
+      "messages": [
+        {
+          "role": "system",
+          "content": "You are a helpful assistant. Answer in crisp and in second person form. First try to answer using the context, and only if it is not good enough, use the internet."
+        }
+      ]
+    };
+
+    List logs;
+    await firestore.collection("users").where("uid", isEqualTo: uid).limit(1).get().then((QuerySnapshot snapshot) {
+      for (var doc in snapshot.docs) {
+        logs = doc["logs"];
+
+        String content = '';
+        for (var log in logs.sublist(logs.length - 6 < 0 ? 0 : logs.length - 6, logs.length)) {
+          content += "On ${log["datetime"]}, I said: ${log["content"]}. ";
+        }
+
+        body["messages"].add({
+          "role": "user",
+          "content": content
+        });
+      }
+    });
+
+    final response = await http.post(Uri.parse(endpoint), headers: <String, String> {
+      "Content-Type": "application/json",
+      "api-key": oaiKey
+    }, body: jsonEncode(body));
+
+    final Map<String, dynamic> data = jsonDecode(response.body) as Map<String, dynamic>;
+
+    return data["choices"][0]["message"]["content"];
+  }
+
+  Future <void> tts(text) async {
+    // somehow convert the text to speech
+  }
+
   void activateSpeechRecognizer() {
     AzureSpeechRecognition.initialize(subKey, region, lang: lang, timeout: "1000");
 
     _speechAzure.setFinalTranscription((text) async {
       if (text[text.length - 1] == "?") {
-
+        final logs = await questionPull();
+        final answer = await runLLM(text, logs);
+        await tts(answer);
       }
       else {
         await statementPush(text);
